d088312f310b5073ce1fe1e9f20fb11e35bae2a2
Kisonya <sskobel04@mail.ru>
2024-09-29 15:16:54 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask
 app = Flask (__name__) #создаем объект
 
 @app.route ("/") #указываем путь
-def start():
+@app.route ("/web") #добавляем еще 1 декоратор-роут,чтобы ф-я срабатывала на разные адреса
+def web():
     return """<!doctype html>
         <html>
            <body>
                <h1>web-сервер на flask</h1>
+               <a href="/author">author</a>
            </body>
+        </html>"""
+
+@app.route ("/author")
+def author():
+    name = "Скобель Софья Валентиновна"
+    group = "ФБИ-21"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p> Студент: """ + name + """</p>
+                <p> Группа: """ + group + """</p>
+                <p> Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
         </html>"""
