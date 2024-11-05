ffb67b22eb798a08934453c114e78766a5007e7d
Kisonya <sskobel04@mail.ru>
2024-11-06 01:27:31 +0700

    Арифметика 2

--- lab4.py
+++ lab4.py
@@ -31,7 +31,7 @@ def add():
     x1 = request.form.get('x1') or 0  # Если поле пустое, принимаем его как 0
     x2 = request.form.get('x2') or 0
     result = int(x1) + int(x2)
-    return render_template('lab4/add.html', x1=x1, x2=x2, result=result)
+    return render_template('lab4/add-form.html', x1=x1, x2=x2, result=result)
 
 
 @lab4.route('/lab4/multiply', methods=['POST'])
@@ -39,7 +39,7 @@ def multiply():
     x1 = request.form.get('x1') or 1  # Если поле пустое, принимаем его как 1
     x2 = request.form.get('x2') or 1
     result = int(x1) * int(x2)
-    return render_template('lab4/multiply.html', x1=x1, x2=x2, result=result)
+    return render_template('lab4/multiply-form.html', x1=x1, x2=x2, result=result)
 
 
 @lab4.route('/lab4/subtract', methods=['POST'])
@@ -47,9 +47,9 @@ def subtract():
     x1 = request.form.get('x1')
     x2 = request.form.get('x2')
     if x1 == '' or x2 == '':
-        return render_template('lab4/subtract.html', error='Оба поля должны быть заполнены!')
+        return render_template('lab4/subtract-form.html', error='Оба поля должны быть заполнены!')
     result = int(x1) - int(x2)
-    return render_template('lab4/subtract.html', x1=x1, x2=x2, result=result)
+    return render_template('lab4/subtract-form.html', x1=x1, x2=x2, result=result)
 
 
 @lab4.route('/lab4/power', methods=['POST'])
@@ -57,8 +57,8 @@ def power():
     x1 = request.form.get('x1')
     x2 = request.form.get('x2')
     if x1 == '' or x2 == '':
-        return render_template('lab4/power.html', error='Оба поля должны быть заполнены!')
+        return render_template('lab4/power-form.html', error='Оба поля должны быть заполнены!')
     if int(x1) == 0 and int(x2) == 0:
-        return render_template('lab4/power.html', error='Оба поля не могут быть равны нулю для операции возведения в степень!')
+        return render_template('lab4/power-form.html', error='Оба поля не могут быть равны нулю для операции возведения в степень!')
     result = int(x1) ** int(x2)
-    return render_template('lab4/power.html', x1=x1, x2=x2, result=result)
+    return render_template('lab4/power-form.html', x1=x1, x2=x2, result=result)
