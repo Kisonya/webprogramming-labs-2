8ddcdce04bef7f50cdcf6722171c862b63d3a712
Kisonya <sskobel04@mail.ru>
2024-11-06 01:23:36 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -25,3 +25,40 @@ def div():
     x2 = int(x2)
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/add', methods=['POST'])
+def add():
+    x1 = request.form.get('x1') or 0  # Если поле пустое, принимаем его как 0
+    x2 = request.form.get('x2') or 0
+    result = int(x1) + int(x2)
+    return render_template('lab4/add.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/multiply', methods=['POST'])
+def multiply():
+    x1 = request.form.get('x1') or 1  # Если поле пустое, принимаем его как 1
+    x2 = request.form.get('x2') or 1
+    result = int(x1) * int(x2)
+    return render_template('lab4/multiply.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/subtract', methods=['POST'])
+def subtract():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/subtract.html', error='Оба поля должны быть заполнены!')
+    result = int(x1) - int(x2)
+    return render_template('lab4/subtract.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/power', methods=['POST'])
+def power():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/power.html', error='Оба поля должны быть заполнены!')
+    if int(x1) == 0 and int(x2) == 0:
+        return render_template('lab4/power.html', error='Оба поля не могут быть равны нулю для операции возведения в степень!')
+    result = int(x1) ** int(x2)
+    return render_template('lab4/power.html', x1=x1, x2=x2, result=result)
