3778cdcf87715c45a6ac1b0a1cf55c1df487d73b
Kisonya <sskobel04@mail.ru>
2024-12-12 14:54:18 +0700

    Удаление фильма

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -15,30 +15,35 @@ function fillFilmList() {
                 let tdYear = document.createElement('td');
                 let tdActions = document.createElement('td');
 
-                // Условие: если названия совпадают, оставить пустым
                 tdTitle.innerText = films[i].title === films[i].title_ru ? '' : films[i].title;
                 tdTitleRus.innerText = films[i].title_ru;
                 tdYear.innerText = films[i].year;
 
-                // Создание кнопок
-                let editButton = document.createElement('button');
-                editButton.innerText = 'Редактировать';
-
                 let delButton = document.createElement('button');
                 delButton.innerText = 'Удалить';
+                delButton.onclick = function () {
+                    deleteFilm(i, films[i].title_ru);
+                };
 
-                // Добавление кнопок в ячейку действий
-                tdActions.append(editButton);
                 tdActions.append(delButton);
 
-                // Добавление ячеек в строку
                 tr.append(tdTitle);
                 tr.append(tdTitleRus);
                 tr.append(tdYear);
                 tr.append(tdActions);
 
-                // Добавление строки в таблицу
                 tbody.append(tr);
             }
+        });
+}
+
+function deleteFilm(id, title) {
+    if (!confirm(`Вы точно хотите удалить фильм "${title}"?`)) {
+        return;
+    }
+
+    fetch(`/lab7/rest-api/films/${id}`, { method: 'DELETE' })
+        .then(function () {
+            fillFilmList(); // Обновляем список фильмов после удаления
         });
 }
