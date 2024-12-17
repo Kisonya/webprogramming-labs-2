e2a8c764d7b90254efb76494af240844772d84f0
Kisonya <sskobel04@mail.ru>
2024-12-18 01:04:51 +0700

    Модель базы данных

--- lab7.py
+++ lab7.py
@@ -27,6 +27,7 @@ def db_connect():
                 database='kisonya_knowledge_base',
                 user='kisonya_knowledge_base',
                 password='123',
+                options='-c client_encoding=WIN1251'  # Устанавливаем клиентскую кодировку
             )
             cur = conn.cursor(cursor_factory=RealDictCursor)
         except Exception as e:
